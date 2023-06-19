a85c02ca19e9b66864f47c5d505dee85fc45b56f
kotaNakm <kotaNakm0317@gmail.com>
2023-06-19 10:57:51 +0900

    add

--- __init__.py
+++ __init__.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from collections import Counter
-from sklearn import model_selection
+from sklearn import model_selection, preprocessing
 from sklearn.model_selection import KFold
 
 
-candidate_col=[
+columns=[
     "Diastolic blood pressure",
     "Heart Rate",
     "Mean blood pressure",
@@ -19,46 +19,69 @@ candidate_col=[
 ]
 
 
-def load_data(data="mixed"):
+def load_data(data="Mixed"):
     fp = os.path.dirname(__file__)
+    sequene_names = os.listdir(f"{fp}/preprocessed/{data}")
+    sequene_names.remove("class.csv")
+    sequene_names = sorted(sequene_names)
 
-    # # Sensor data
-    # data = pd.read_csv(fp + "/PdM_telemetry.csv.gz")
+    data_list = []
+    class_list = []
 
-    # # Error alarm logs
-    # data = data.merge(
-    #     pd.read_csv(fp + "/PdM_errors.csv.gz"), how="left", on=["datetime", "machineID"]
-    # )
+   # Encode categorical class label
+    oe = preprocessing.OrdinalEncoder()
+    class_df = pd.read_csv(f"{fp}/preprocessed/{data}/class.csv")
+    class_df["Class_encoded"] = oe.fit_transform(class_df["Class"])
 
-    # # Failure logs
-    # data = data.merge(
-    #     pd.read_csv(fp + "/PdM_failures.csv.gz"),
-    #     how="left",
-    #     on=["datetime", "machineID"],
-    # )
 
-    # # Formatting
-    # data.datetime = pd.to_datetime(data.datetime)
+    for patient_id, name in enumerate(sequene_names):
+        class_id = class_df[class_df["stay"] == name]["Class_encoded"]
+        temp_df = pd.read_csv(f"{fp}/preprocessed/{data}/{name}")
+        temp_df["patient_id"] = patient_id
+        temp_df["Class"] = class_id
+        data_list.append(temp_df)
+
+    data_df = pd.concat(data_list,axis=0)
 
     return data
 
 
 def cleaning(df):
+    df = df[["Hours","patient_id","Class"] + columns]
 
-    # NaN values are encoded to -1
-    df = df.sort_values("errorID")
-    df.errorID = df.errorID.factorize()[0]
-    df = df.sort_values("failure")
-    df.failure = df.failure.factorize()[0]
-    df = df.sort_values(["machineID", "datetime"])
-
-    df.errorID = df.errorID.astype("int")
-    df.failure = df.failure.astype("int")
-
-    df.volt = df.volt.astype("float32")
-    df.rotate = df.rotate.astype("float32")
-    df.pressure = df.pressure.astype("float32")
-    df.vibration = df.vibration.astype("float32")
-
-    df.datetime = pd.to_datetime(df.datetime)
     return df
+
+def add_rul(concatenated_df):
+    label_name = "Time_to_Event" 
+    df_list = []
+    for id_, df in concatenated_df.groupby("patient_id"):
+        df = df.sort_values("Hours")
+        time_array = np.arange(len(df))[::-1]
+        df[label_name] = time_array
+        df_list.append(df)
+        
+    return df_list
+
+
+def load_clean_data_rul_k_folds(
+    data,
+    split_ind,
+    k=5,
+    random_state=0,
+):
+    df_list = add_rul((cleaning(load_data(data=data))))
+
+    data_index = range(len(df_list))
+
+    kf = KFold(
+        n_splits=k,
+        random_state=random_state,
+        shuffle=True,
+    )
+
+    train_idx, test_idx = list(kf.split(data_index))[split_ind]
+
+    train_df_list = [df_list[i] for i in train_idx]
+    test_df_list = [df_list[i] for i in test_idx]
+
+    return train_df_list, test_df_list
